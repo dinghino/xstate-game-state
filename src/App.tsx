1181fa59cdf18fd656363beb2e6d4b7a569e0a3f
dinghino <dinghino@gmail.com>
2022-09-10 23:49:17 +0200

    🦄 move toolbar into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
 import React, { Suspense } from "react";
 import { useSelector } from "@xstate/react";
 import {
-  ActionIcon,
   AppShell,
-  Button,
-  Center,
   ColorSchemeProvider,
   Container,
-  Group,
-  Header,
   MantineProvider,
   MantineTheme,
   Navbar,
-  Stack,
 } from "@mantine/core";
 import { closeAllModals, ModalsProvider } from "@mantine/modals";
-import { CanvasRoot } from "./components/threejs";
-
-// import service, { playerService } from "./state";
-import { playerService } from "./state";
-import { useControlsModal } from "./hooks/use-controls-modal";
-import { DeviceGamepad2, Keyboard, Mouse, TableAlias } from "tabler-icons-react";
-import { TControllerType } from "./machines/configuration/configuration.types";
-import { usePauseMouse } from "./hooks/use-pause-mouse";
-import { VelocityStats } from "./components/VelocityStats";
 import { useWindowEvent } from "@mantine/hooks";
+
+import { playerService } from "./state";
+import { usePauseMouse } from "./hooks/use-pause-mouse";
+
+import { CanvasRoot } from "./components/threejs";
+import { VelocityStats } from "./components/VelocityStats";
+import { Toolbar } from "./components/Toolbar";
 
 const Provider: React.FC<{children: React.ReactNode}> = ({ children }) => (
   <ColorSchemeProvider colorScheme="dark" toggleColorScheme={() => null}>
@@ -35,51 +27,6 @@ const Provider: React.FC<{children: React.ReactNode}> = ({ children }) => (
     </MantineProvider>
   </ColorSchemeProvider>
 );
-
-const Toolbar = () => {
-  const inputs = useSelector(playerService, ({ context }) => context.inputs);
-  const active = useSelector(inputs, (state) => state.matches('active'));
-  const controllers = useSelector(inputs, ({ context }) => context.controllers);
-
-  const showControlsModal = useControlsModal(inputs as any);
-
-  const toggleService = React.useCallback(() => {
-    inputs.send(active ? "STOP" : "START")
-  }, [inputs.send, active]);
-
-  const toggleController = React.useCallback((controller: TControllerType) => {
-    inputs.send({ type: "TOGGLE_CONTROLLER", controller, value: !controllers[controller]})
-  }, [controllers])
-  const activeColor = (v?: boolean) => v ? 'green' : 'red'
-
-  return (
-    <Stack align="right">
-      <Button uppercase leftIcon={<DeviceGamepad2 size={18}/>} onClick={toggleService} color={activeColor(active)}>
-        {active ? "Running" : "Stopped"}
-      </Button>
-      <Group position="center" grow>
-        <ActionIcon
-          disabled={!active}
-          size="lg" variant="filled"
-          onClick={() => toggleController('keyboard')}
-          color={activeColor(controllers.keyboard)}
-          >
-          <Keyboard />
-        </ActionIcon>
-        <ActionIcon
-          disabled={!active}
-          size="lg" variant="filled"
-          onClick={() => toggleController('mouse')}
-          color={activeColor(controllers.mouse)}>
-          <Mouse />
-        </ActionIcon>
-        <ActionIcon variant="filled" size="lg" onClick={showControlsModal}>
-        <TableAlias/>
-      </ActionIcon>
-      </Group>
-    </Stack>
-  )
-}
 
 export default function App() {
   const inputs = useSelector(playerService, ({ context }) => context.inputs);
@@ -112,9 +59,9 @@ export default function App() {
     <Provider>
       <AppShell
         navbar={
-          <Navbar width={{base:200}} p="xs" py="xl">
+          <Navbar width={{base:240}} p="xs" py="xl">
             <Navbar.Section mb="xl"><Toolbar/></Navbar.Section>
-            <Navbar.Section><VelocityStats vertical /></Navbar.Section>
+            <Navbar.Section><VelocityStats /></Navbar.Section>
           </Navbar>}
         styles={getStyles}
       >
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useSelector } from "@xstate/react";
+import {
+  ActionIcon,
+  Button,
+  Group,
+  Stack,
+} from "@mantine/core";
+// import service, { playerService } from "./state";
+import { playerService } from "../state";
+import { useControlsModal } from "../hooks/use-controls-modal";
+import { DeviceGamepad2, Keyboard, Mouse, TableAlias } from "tabler-icons-react";
+import { TControllerType } from "../machines/configuration/configuration.types";
+
+export const Toolbar: React.FC = () => {
+  const inputs = useSelector(playerService, ({ context }) => context.inputs);
+  const active = useSelector(inputs, (state) => state.matches('active'));
+  const controllers = useSelector(inputs, ({ context }) => context.controllers);
+
+  const showControlsModal = useControlsModal(inputs as any);
+
+  const toggleService = React.useCallback(() => {
+    inputs.send(active ? "STOP" : "START")
+  }, [inputs.send, active]);
+
+  const toggleController = React.useCallback((controller: TControllerType) => {
+    inputs.send({ type: "TOGGLE_CONTROLLER", controller, value: !controllers[controller]})
+  }, [controllers])
+  const activeColor = (v?: boolean) => v ? 'green' : 'red'
+
+  return (
+    <Stack>
+      <Button uppercase leftIcon={<DeviceGamepad2 size={18}/>} onClick={toggleService} color={activeColor(active)}>
+        {active ? "Running" : "Stopped"}
+      </Button>
+      <Group position="center" grow>
+        <ActionIcon
+          disabled={!active}
+          size="lg" variant="filled"
+          onClick={() => toggleController('keyboard')}
+          color={activeColor(controllers.keyboard)}
+          >
+          <Keyboard />
+        </ActionIcon>
+        <ActionIcon
+          disabled={!active}
+          size="lg" variant="filled"
+          onClick={() => toggleController('mouse')}
+          color={activeColor(controllers.mouse)}>
+          <Mouse />
+        </ActionIcon>
+        <ActionIcon variant="filled" size="lg" onClick={showControlsModal}>
+        <TableAlias/>
+      </ActionIcon>
+      </Group>
+    </Stack>
+  )
+}
