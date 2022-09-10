1cf28179d56ca2e6f7429873cb98e3a0e42c6266
dinghino <dinghino@gmail.com>
2022-09-10 14:51:24 +0200

    🧑‍💻 Add getAxisFromKeybidings function
    allows you to get ALL the axis controlled by an individual TControllerType from config.keybidings

---
+++ src/machines/functions/get-axis-from-keybidings.ts
@@ -0,0 +1,12 @@
+import { objEntries } from "../../utils"
+import { TControllerType } from "../configuration/configuration.types"
+
+export const getAxisFromKeybindings = <
+  Axis extends string,
+  Actions extends string,
+  Key extends `${TControllerType}.${string}`
+>(bindings: Record<Key, Axis | Actions>, controller: TControllerType): Array<Axis|Actions> => {
+  return objEntries(bindings)
+    .filter(([key]) => key.split('.')[0] === controller)
+    .map(([, value]) => value).filter((v, i, a) => a.indexOf(v) === i)
+}
--- src/machines/functions/index.ts
+++ src/machines/functions/index.ts
@@ -1,3 +1,4 @@
 export * from "./inputs.functions";
 export * from "./for-each-input-configuration";
 export * from "./is-event-type-guard";
+export * from './get-axis-from-keybidings';
