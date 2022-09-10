ffc789622cae1fbd868a28fedd03347a7dc0037f
dinghino <dinghino@gmail.com>
2022-09-10 19:58:23 +0200

    ♻️ move player business logic inside player component

--- src/components/threejs/Canvas.tsx
+++ src/components/threejs/Canvas.tsx
@@ -1,90 +1,22 @@
 import React from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Mesh, Object3D, Vector3 } from "three";
+import { Mesh, Vector3 } from "three";
 
 // import { rotateOnWorldAxisWithInputs } from "../../functions/three";
 
-import { Cube } from "./Cube";
+import { Player } from "./Player.mesh";
 import { useControlsModal } from "../../hooks/use-controls-modal";
 
 import { /*inputService */ playerService } from "../../state";
 import { useSelector } from "@xstate/react";
 import { PerspectiveCamera } from "@react-three/drei";
-import { useWindowEvent } from "@mantine/hooks";
-
-const AX = new Vector3(1, 0, 0);
-const AY = new Vector3(0, 1, 0);
-const AZ = new Vector3(0, 0, 1);
-
-// const state = interpret(stateMachine);
-
-interface Values {
-  yaw: number;
-  pitch: number;
-  roll: number;
-  forward: number;
-  left: number;
-  up: number;
-}
-
-function updateObjectTransform(object: Object3D, inputs: Values, dt: number) {
-  if (!object) return;
-  object.rotateOnWorldAxis(AY, inputs.yaw * dt);
-  object.rotateOnWorldAxis(AX, inputs.pitch * dt);
-  object.rotateOnWorldAxis(AZ, (Math.PI / 2) * inputs.roll * dt);
-  object.translateX(inputs.left * dt);
-  object.translateY(inputs.up * dt);
-  object.translateZ(inputs.forward * dt);
-}
-
-function centerObject(object: Object3D) {
-  if (!object) return;
-  object.position.x = 0;
-  object.position.y = 0;
-  object.position.z = 0;
-  object.rotation.x = 0;
-  object.rotation.y = 0;
-  object.rotation.z = 0;
-}
-
-// function parseTransform(values: Vector3|Euler): [number, number, number] {
-function parseTransform(obj: Object3D): {position: [number, number, number], rotation: [number, number, number]} {
-  const p = obj.position;
-  const r = obj.rotation;
-  return { position: [p.x, p.y, p.z], rotation: [r.x, r.y, r.z] }
-}
 
 function Scene() {
-  const cube = React.useRef<Mesh>(null!);
-
-  const inputs = useSelector(playerService, ({ context }) => context.inputs);
-  const state = useSelector(playerService, ({ context }) => context.values);
-
-  const values = useSelector(inputs, ({ context }) => context.values);
-  const velocity = useSelector(state, ({ context }) => context.velocity);
-
-  useFrame((_, dt) => {
-    const obj = cube.current;
-    state.send({ type: "UPDATE", values });
-    updateObjectTransform(obj, velocity, dt);
-    state.send({ type: 'UPDATE_TRANSFORM', ...parseTransform(obj) });
-  });
-
-  useWindowEvent("keypress", (e) => {
-    if (e.code === "KeyT") {
-      centerObject(cube!.current);
-      state.send("RESET");
-    }
-    if (e.code === "KeyZ") {
-      state.send("DEBUG");
-    }
-  });
-
   return (
     <>
       <ambientLight intensity={0.05} />
       <directionalLight color="red" position={[1, 3, 5]} intensity={2} />
-      <Cube ref={cube} />
+      <Player />
     </>
   );
 }
@@ -94,6 +26,7 @@ export const CanvasRoot: React.FC = () => {
   const inputs = useSelector(playerService, ({ context }) => context.inputs);
 
   const showControlsModal = useControlsModal(inputs as any);
+
   return (
     <Canvas
       camera={{ position: [0, 1, 3] }}
@@ -102,14 +35,43 @@ export const CanvasRoot: React.FC = () => {
         height: "100%",
       }}
       onCreated={({ gl }) => {
-        gl.setClearColor("#000");
+        // gl.setClearColor("#000");
         showControlsModal();
       }}
     >
-      <PerspectiveCamera position={[0, 3, 5]} ref={camera} fov={45} />
+      <PerspectiveCamera position={[0, 3, 5]} ref={camera} fov={90} />
       <gridHelper />
       <axesHelper />
       <Scene />
     </Canvas>
   );
 };
+
+/** Demo for remote player client receiving transform.
+ * In this example the position is evaluated directly from the player's inputs
+ * plus some shift. In a real context the values are received through websocket,
+ * either directly in the component or through a state service (clientsMachine?)
+ * and handled with a player/client id as a ref.
+ */
+const OtherPlayer = () => {
+  const player = React.useRef<Mesh>(null!);
+
+  const state = useSelector(playerService, ({ context }) => context.values);
+  const { position, rotation } = useSelector(state, ({ context }) => context.transform);
+  useFrame(() => {
+    const obj = player.current;
+    if (!obj) return;
+    obj.position.x = position[0];
+    obj.position.y = position[1] + 0.5;
+    obj.position.z = position[2] + 1;
+    obj.rotation.x = rotation[0]
+    obj.rotation.y = rotation[1]
+    obj.rotation.z = rotation[2]
+  });
+  return (
+    <mesh ref={player} position={[0,0,0]} scale={0.5}>
+      <boxGeometry />
+      <meshBasicMaterial color="#33ffff"/>
+    </mesh>
+  )
+}
---
+++ src/components/threejs/Player.mesh.tsx
@@ -0,0 +1,76 @@
+import { useWindowEvent } from "@mantine/hooks";
+import { MeshProps, useFrame } from "@react-three/fiber";
+import { useSelector } from "@xstate/react";
+import React from "react";
+import { Mesh, Object3D, Vector3 } from "three";
+import { playerService } from "../../state";
+interface Values {
+  yaw: number;
+  pitch: number;
+  roll: number;
+  forward: number;
+  left: number;
+  up: number;
+}
+
+const AX = new Vector3(1, 0, 0);
+const AY = new Vector3(0, 1, 0);
+const AZ = new Vector3(0, 0, 1);
+
+// export const initialRotation = new Euler(degToRad(45), 0, degToRad(45));
+function parseTransform(obj: Object3D): {position: [number, number, number], rotation: [number, number, number]} {
+  const p = obj.position;
+  const r = obj.rotation;
+  return { position: [p.x, p.y, p.z], rotation: [r.x, r.y, r.z] }
+}
+
+function updateObjectTransform(object: Object3D, inputs: Values, dt: number) {
+  if (!object) return;
+  object.rotateOnWorldAxis(AY, inputs.yaw * dt);
+  object.rotateOnWorldAxis(AX, inputs.pitch * dt);
+  object.rotateOnWorldAxis(AZ, (Math.PI / 2) * inputs.roll * dt);
+  object.translateX(inputs.left * dt);
+  object.translateY(inputs.up * dt);
+  object.translateZ(inputs.forward * dt);
+}
+function centerObject(object: Object3D) {
+  if (!object) return;
+  object.position.x = 0;
+  object.position.y = 0;
+  object.position.z = 0;
+  object.rotation.x = 0;
+  object.rotation.y = 0;
+  object.rotation.z = 0;
+}
+
+
+export const Player: React.FC<MeshProps> = ((props) => {
+  const cube = React.useRef<Mesh>(null!);
+
+  const inputs = useSelector(playerService, ({ context }) => context.inputs);
+  const state = useSelector(playerService, ({ context }) => context.values);
+
+  const values = useSelector(inputs, ({ context }) => context.values);
+  const velocity = useSelector(state, ({ context }) => context.velocity);
+
+  useWindowEvent("keypress", (e) => {
+    if (e.code === "KeyT") {
+      centerObject(cube!.current);
+      state.send("RESET");
+    }
+  });
+
+  useFrame((_, dt) => {
+    const obj = cube.current;
+    state.send({ type: "UPDATE", values });
+    updateObjectTransform(obj, velocity, dt);
+    state.send({ type: 'UPDATE_TRANSFORM', ...parseTransform(obj) });
+  });
+
+  return (
+    <mesh {...props} ref={cube}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  );
+});
