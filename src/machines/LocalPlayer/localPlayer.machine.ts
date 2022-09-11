3d574fdddf4ce0432f1387c9b6bab6cdf0ad068d
dinghino <dinghino@gmail.com>
2022-09-11 21:26:33 +0200

    :bug: fix start|stop forwarding actions

--- src/machines/LocalPlayer/localPlayer.machine.ts
+++ src/machines/LocalPlayer/localPlayer.machine.ts
@@ -6,7 +6,6 @@ import type { StateAxisSettings } from '../ShipState/shipState.types'
 import { createControlsMachine } from '../Controls'
 import { createShipStateMachine } from '../ShipState/shipState.machine'
 import { LocalPlayerContext, LocalPlayerEvent } from './localPlayer.types'
-import { forwardTo } from 'xstate/lib/actions'
 
 export function createLocalPlayerMachine<
   Axis extends string,
@@ -72,10 +71,12 @@ export function createLocalPlayerMachine<
     },
     {
       actions: {
-        startInputs: forwardTo((ctx) => ctx.inputs ),
-        stopInputs: forwardTo((ctx) => ctx.inputs ),
-        startState: forwardTo((ctx) => ctx.values ),
-        stopState: forwardTo((ctx) => ctx.values ),
+
+        startInputs: send({ type: 'START' }, { to: (ctx) => ctx.inputs }),
+        stopInputs: send({ type: 'STOP' }, { to: (ctx) => ctx.inputs }),
+
+        startState: send({ type: 'START' }, { to: (ctx) => ctx.values }),
+        stopState: send({ type: 'STOP' }, { to: (ctx) => ctx.values }),
 
         updateValuesFromInputs: send(
           ( {inputs }) => ({ type: 'UPDATE', values: inputs.getSnapshot().context.values }),
